6b2eb13c8e1b4e8be60569a29b10eb6fba59d9d8
cesswhite <acessloop@gmail.com>
2024-04-13 10:09:13 -0600

    Mininaml changes

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full relative h-full pt-12 bg-zinc-50 dark:bg-zinc-950">
-    <img src="/shadow.png"
-      class="fixed object-cover object-center w-full h-full left-0 top-0 z-10 dark:mix-blend-soft-light opacity-50"
+    <img v-motion-fade src="/shadow.png"
+      class="object-cover object-center fixed w-full h-full left-0 top-0 z-10 dark:mix-blend-soft-light"
       alt="shadow-background" loading="lazy">
     <div class="relative z-20">
       <SwitchMode />
       <UContainer>
-        <div class="flex flex-col gap-2 items-center justify-center">
+        <div v-motion-fade class="flex flex-col gap-2 items-center justify-center">
           <PhotoProfile />
           <MyInfo />
           <SocialNetwork />
--- components/SocialNetwork.vue
+++ components/SocialNetwork.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="flex items-center gap-2">
+    <div class="flex items-center gap-4">
         <NuxtLink v-if="_instagram" :to="_instagram" target="_blank">
             <UIcon name="i-simple-icons-instagram"
-                class="text-xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
                 dynamic />
         </NuxtLink>
         <NuxtLink v-if="_x" :to="_x" target="_blank">
             <UIcon name="i-simple-icons-x"
-                class="text-xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
                 dynamic />
         </NuxtLink>
         <NuxtLink v-if="_facebook" :to="_facebook" target="_blank">
             <UIcon name="i-simple-icons-facebook"
-                class="text-xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
                 dynamic />
         </NuxtLink>
         <NuxtLink v-if="_github" :to="_github" target="_blank">
             <UIcon name="i-simple-icons-github"
-                class="text-xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
                 dynamic />
         </NuxtLink>
         <NuxtLink v-if="_tiktok" :to="_tiktok" target="_blank">
             <UIcon name="i-simple-icons-tiktok"
-                class="text-xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
+                dynamic />
+        </NuxtLink>
+        <NuxtLink v-if="website" :to="website" target="_blank">
+            <UIcon name="i-heroicons-globe-alt"
+                class="text-2xl text-zinc-800/60 dark:text-zinc-50/60 dark:hover:text-zinc-50 hover:text-zinc-950"
                 dynamic />
         </NuxtLink>
     </div>
